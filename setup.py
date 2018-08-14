416ff3ac8cdc2922d7a341b5bbdf9ca7d7035dc8
siopow sisig <ievsantillan@gmail.com>
2018-08-14 18:28:33 -0400

    initial data exploration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Crime Analysis using Toronto's Open Crime Data',
+    author='ian santillan',
+    license='MIT',
+)
